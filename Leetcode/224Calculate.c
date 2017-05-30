17866cb497c3e2d0240ed0b92e8377ee6e638607
ya-ning <wangyaning6166@gmail.com>
2017-05-30 23:13:01 +0800

    224 A simple calculator

---
+++ Leetcode/224Calculate.c
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: 224Calculate.c
+	> Author: Yaning Wang, CS1607
+	> Mail: wangyanign6166@gmail.com
+	> School: HUST
+	> Created Time: 2017年05月30日 星期二 21时27分34秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int calculate(char* s) {
+    int res = 0;
+    char ch = 0;
+    while ((ch = *s++)) {
+    }
+    
+    
+    return res;
+}
+
+int main(void) {
+    char* str = "1 + 1";
+
+
+
+
+    return 0;
+}
