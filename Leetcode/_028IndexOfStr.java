f1d30a728f93c08cf34483df2d400002580cdd42
ya-ning <wangyaning6166@gmail.com>
2017-06-10 23:29:17 +0800

    KMP算法也可以，方便起见还是java内置吧

---
+++ Leetcode/_028IndexOfStr.java
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: _028IndexOfStr.java
+	> Author: Yaning Wang, CS1607
+	> Mail: wangyanign6166@gmail.com
+	> School: HUST
+	> Created Time: 2017年06月10日 星期六 23时28分10秒
+ ************************************************************************/
+
+public class _028IndexOfStr{
+    public class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+
+    }
+}
+
