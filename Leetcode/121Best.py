dd4b61b589554d216a89da2b667dcccf48da6bf1
ya-ning <wangyaning6166@gmail.com>
2017-07-02 12:08:53 +0800

    python版本，速度好慢啊

---
+++ Leetcode/121Best.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        maxPro = 0
+        minPri = 102400
+        temp = 0
+        for x in prices:
+            maxPro = max(x - minPri, maxPro)
+            minPri = min(x, minPri)
+        return maxPro
+        
