9633ab74525f3bddf95b89bc056483e83358bd76
ya-ning <wangyaning6166@gmail.com>
2017-07-03 12:53:25 -0400

    linux

---
+++ Leetcode/167TwoSum.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using std::vector;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        std::vector<int> sol;
+        for (int i = 0; i < numbers.size(); ++i) {
+        	
+        }
+    }
+};
