7f5b3d76afbdd655df351d7cc8336f3bbf7ff3ee
ya-ning <wangyaning6166@gmail.com>
2017-07-04 11:42:13 +0800

    2个数字和

--- Leetcode/167TwoSum.cpp
+++ Leetcode/167TwoSum.cpp
@@ -3,9 +3,20 @@ using std::vector;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        std::vector<int> sol;
-        for (int i = 0; i < numbers.size(); ++i) {
-        	
+        int low = 0;
+        int high = numbers.size() - 1;
+        int sum = 0;
+        // 妙啊，夹挤，厉害
+        while (low < high) {
+            sum = numbers[low] + numbers[high];
+            if (sum > target) {
+                hight--
+            } else if (sum < target) {
+                low++;
+            } else {
+                return {low + 1, high + 1};
+            }
         }
+        return {-1,-1};
     }
 };
