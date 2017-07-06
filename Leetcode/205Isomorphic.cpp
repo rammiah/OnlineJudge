4d85429c5e572c30798386b3afbb966eb7edf8c5
YaNing <wangyaning6166@gmail.com>
2017-07-06 09:56:35 +0800

    Update 205Isomorphic.cpp

--- Leetcode/205Isomorphic.cpp
+++ Leetcode/205Isomorphic.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-<<<<<<< HEAD
-    	// 开始以为只有26个字┭┮﹏┭┮
-=======
-    	// 开始以为只有26个字幕┭┮﹏┭┮
->>>>>>> 03313d4ebc2d17ecde78fa6334bb6c9922af68b3
+    	// 开始以为只有26个字母┭┮﹏┭┮
     	const int base = 256;
         if (s.length() != t.length())return false;
         int*char_s = new int[base];
@@ -17,15 +13,10 @@ public:
         for (int i = 0; i < s.length(); ++i) {
  
            	if (char_s[s[i]] == -1 && char_t[t[i]] == -1){
-           		char_s[s[i]] = char_t[t[i]] = ++count;
+           		char_s[s[i]] = char_t[t[i]] = count++;
            	} else if (char_s[s[i]] != char_t[t[i]]) {
            		return false;
            	}
         }
         return true;
     }
-<<<<<<< HEAD
-};
-=======
-};
->>>>>>> 03313d4ebc2d17ecde78fa6334bb6c9922af68b3
