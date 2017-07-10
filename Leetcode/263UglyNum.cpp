d7daabf9d11421e94ae818a0466b17f9b7eceaeb
ya-ning <wangyaning6166@gmail.com>
2017-07-10 22:10:54 +0800

    负数直接判定不是

---
+++ Leetcode/263UglyNum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        // 负数是不算是素数的
+        if (num <= 0) return false;
+        if (num == 1) return true;
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        if (num != 1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+};
