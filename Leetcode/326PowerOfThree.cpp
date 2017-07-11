376eefd7f87994f1044edb03d86c7ec0625621be
ya-ning <wangyaning6166@gmail.com>
2017-07-11 10:49:58 +0800

    Power of three, add comment

--- Leetcode/326PowerOfThree.cpp
+++ Leetcode/326PowerOfThree.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if (n <= 0) return false;
+        // 这个数是3的在int里最大的幂值，用py算的，只要是3的幂就一定能整除
         return 3486784401 % n == 0;
     }
 };
