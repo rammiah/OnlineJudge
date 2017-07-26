5d5d591e95f3e399b2b878e4530629828639c988
ya-ning <wangyaning6166@gmail.com>
2017-07-26 13:03:19 +0800

    016 3sum

--- Leetcode/016_3SumClose.cpp
+++ Leetcode/016_3SumClose.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        
+        int minDiff = INT_MAX;
+        int left = 0;
+        int right = nums.size() - 1;
+        int diff = 0;
+        for (int i = 1; i < nums.size() - 1; ++i) {
+            left = 0;
+            right = nums.size() - 1;
+            while (left < i && right > i) {
+                diff = nums[left] + nums[right] + nums[i] - target;
+                if (abs(diff) < abs(minDiff)) {
+                    minDiff = diff;
+                }
+                if (diff < 0) {
+                    left++;
+                } else if (diff > 0) {
+                    right--;
+                } else {
+                    return target;
+                }
+            }
+        }
+        return target + minDiff;
     }
 };
