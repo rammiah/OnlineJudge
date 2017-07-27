e4381a450d099d5380ca49f3a3f7d9549ae780f2
ya-ning <wangyaning6166@gmail.com>
2017-07-27 23:11:31 +0800

    第一个SQL

---
+++ Leetcode/175SQLSelect.sql
@@ -0,0 +1,26 @@
+# Write your MySQL query statement below
+# 一句简单的SQL语句
+SELECT FirstName, LastName, City, State
+FROM Person LEFT JOIN Address 
+ON Person.PersonID = Address.PersonID;
+
+-- Table: Person
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId is the primary key column for this table.
+-- Table: Address
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId is the primary key column for this table.
