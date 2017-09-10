6d80a660084492eccee68fa3e7bf324237683647
hustr <wangyaning6166@gmail.com>
2017-09-10 23:29:55 +0800

    update

--- Leetcode/_062Paths.java
+++ Leetcode/_062Paths.java
@@ -1,24 +1,26 @@
 public class Solution {
     // 有必要再优化一下
     public static long sum(int m, int n) {
-        int[][] temp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            temp[i][0] = 1;
-        }
+        int[] first = new int[n], second = new int[n];
         for (int i = 0; i < n; i++) {
-            temp[0][i] = 1;
+            first[i] = 1;
         }
+        int[] temp = null;
         for (int i = 1; i < m; i++) {
+            second[0] = 1;
             for (int j = 1; j < n; j++) {
-                temp[i][j] = temp[i - 1][j] + temp[i][j - 1];
+                second[j] = second[j - 1] + first[j];
             }
+            temp = first;
+            first = second;
+            second = temp;
         }
 
-        return temp[m - 1][n - 1];
+        return first[n - 1];
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(10, 10));
+        System.out.println(sum(2, 2));
     }
 }
 
