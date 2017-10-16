64ac3e528386015e88c4f679a118834a0f2d374f
hustr <wangyaning6166@gmail.com>
2017-10-16 22:47:32 +0800

    subsets

---
+++ Leetcode/090SubSets.cpp
@@ -0,0 +1,22 @@
+// [1, 2, 2]
+// [
+//     [2],
+//     [1],
+//     [1,2,2],
+//     [2,2],
+//     [1,2],
+//     []
+//   ]
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int>temp;
+
+
+        return result;
+    }
+};
