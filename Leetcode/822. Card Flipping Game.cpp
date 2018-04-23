f2b4d0ed37ceae50fc9512fbd8c7613989dfab5b
hustr <wangyaning6166@gmail.com>
2018-04-23 19:47:19 +0800

    update

--- Leetcode/822. Card Flipping Game.cpp
+++ Leetcode/822. Card Flipping Game.cpp
@@ -15,18 +15,21 @@ public:
         }
 
         for (int i = 0; i < card_cnt; ++i) {
+            // 数字存在
             have[fronts[i]] = have[backs[i]] = 1;
             if (fronts[i] == backs[i]) {
+                // fronts[i]必定存在正反面
                 possible[fronts[i]] = 1;
             }
         }
 
         for (int i = 1; i < 2001; ++i) {
+            // 第一个就是
             if (possible[i] == 0 && have[i] == 1) {
                 return i;
             }
         }
-
+        // 没有满足需求的
         return 0;
     }
 };
