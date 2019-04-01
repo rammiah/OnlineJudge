d92ef545e9f832cfd83f48e2d4792f0043696efc
Yaning Wang <wangyaning6166@gmail.com>
2019-04-01 19:44:02 +0800

    use less mem

--- Leetcode/973. K Closest Points to Origin.cpp
+++ Leetcode/973. K Closest Points to Origin.cpp
@@ -8,21 +8,28 @@ class Solution {
 		}
 	};
 
-	struct comp {
+	struct Comp {
 		bool operator()(const Point& a, const Point& b) {
-			return a.dist > b.dist;
+			return a.dist < b.dist;
 		}
 	};
 
 public:
 	vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-		std::priority_queue<Point, std::vector<Point>, comp> q;
+		std::priority_queue<Point, std::vector<Point>, Comp> q;
+		Comp comp;
 		for (auto& p : points) {
-			q.emplace(p[0], p[1]);
+			if (q.size() < K) {
+				q.emplace(p[0], p[1]);
+
+			}
+			else if (comp(Point(p[0], p[1]), q.top())) {
+				q.pop();
+				q.emplace(p[0], p[1]);
+			}
 		}
 		std::vector<std::vector<int>> res;
-		while (K > 0) {
-			--K;
+		while (!q.empty()) {
 			auto& p = q.top();
 			res.push_back(std::vector<int>{p.x, p.y});
 			q.pop();
